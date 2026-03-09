eeb764e4c17190a5eb26e984f4eef36f61955f33
Squallqt <admin.squall@free.fr>
2026-03-09 17:40:09 +0100

    feat: add multiplayer support and fuel HUD

--- scripts/ADS_Specialization.lua
+++ scripts/ADS_Specialization.lua
@@ -6232,7 +6232,6 @@ function AdvancedDamageSystem.ConsoleCommands:setService(rawArgs)
     end
     
     spec.serviceLevel = value
-    vehicle:addEntryToMaintenanceLog(AdvancedDamageSystem.STATUS.MAINTENANCE, AdvancedDamageSystem.MAINTENANCE_TYPES.STANDARD, "NONE", false, 0, true)
     print(string.format("ADS: Set Service level for '%s' to %.2f.", vehicle:getFullName(), value))
 end
 
@@ -6249,7 +6248,6 @@ function AdvancedDamageSystem.ConsoleCommands:resetVehicle()
     spec.conditionLevel = 1.0
     spec.serviceLevel = 1.0
     vehicle:removeBreakdown()
-    vehicle:addEntryToMaintenanceLog(AdvancedDamageSystem.STATUS.MAINTENANCE, AdvancedDamageSystem.MAINTENANCE_TYPES.STANDARD, "NONE", false, 0, true)
     print(string.format("ADS: Fully reset state for '%s'.", vehicle:getFullName()))
 end
 
